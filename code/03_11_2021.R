bfea7b9c243b301534eb1768c35d49f9efb725cd
soumikp <soumikp@umich.edu>
2021-03-11 13:40:12 -0500

    quarterly survey data cleaned

--- code/03_11_2021.R
+++ code/03_11_2021.R
@@ -1,17 +1,16 @@
 require(tidyverse)
 
-#cleaning intake data - promis scores summed
-promis.intake.cleaner <- function(data){
-  
+#cleaning quarterly data - promis scores summed
+promis.quarter.cleaner <- function(data){
   temp <- data %>% 
-    filter(SurveyTaskStatus == "Complete", 
-           SurveyName == "NIH PROMIS Cognitive Function 4a - Intake") %>% 
+    filter(SurveyName == "NIH PROMIS Cognitive Function 4a - Quarterly",
+           SurveyTaskStatus == "Complete") %>% 
     select(ParticipantResearchID, SurveyEndDate, SurveyQuestion, SurveyAnswer)
-  
-  if(temp %>% nrow() == 0){
+    if(temp %>% nrow() == 0){
     return(NULL)
   }else{
     return(temp %>% 
+             unique() %>% 
              pivot_wider(names_from = SurveyQuestion,
                          values_from = SurveyAnswer) %>% 
              select(contains(c("ID", "Date", "In the past"))) %>% 
@@ -21,11 +20,11 @@ promis.intake.cleaner <- function(data){
                     X2 = "In the past 7 days:I have had to work harder than usual to keep track of what I was doing",
                     X3 = "In the past 7 days:I have had trouble shifting back and forth between different activities that require thinking",
                     X4 = "In the past 7 days:My thinking has been slow") %>% 
-             mutate(promis = (as.numeric(X1) + as.numeric(X2) + as.numeric(X3) + as.numeric(X4))) %>% 
+             mutate(promis =  as.numeric(X1) + as.numeric(X2) + as.numeric(X3) + as.numeric(X4)) %>% 
              select(-contains("X")))
   }
-  }
-  
+}
+
 data <- NULL
 phrase <- "/nfs/turbo/umms-HDS629/MIPACT/HealthKit_Live/Surveys/Surveys_20"
 for (i in c("17", "18", "19", "20")){
@@ -34,7 +33,10 @@ for (i in c("17", "18", "19", "20")){
       next
     }else{
       data <- rbind(data, 
-                    promis.intake.cleaner(read_csv(paste(phrase, i, j, ".csv"))))
+                    promis.quarter.cleaner(read_csv(paste0(phrase, i, j, ".csv"))))
     }
   }
 }
+
+write_csv(data, "quarterly.csv")
+
